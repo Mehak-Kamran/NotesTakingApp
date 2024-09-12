34bfe115d5e31ffe9df0078883bc05c51298b25e
Mehak Kamran <mehakkamran52@gmail.com>
2024-09-12 11:31:11 +0500

    handling form storing data from dorm into files and displaying inform of cards

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 //boiler plate for express
 
 //req
-let express=require("express");
-let path=require("path")
-let fs=require("fs")
+let express = require("express");
+let path = require("path")
+let fs = require("fs")
 //invoke
-let app=express();
+let app = express();
 //set viewengine middleware
-app.set("view engine","ejs")
+app.set("view engine", "ejs")
 //set up cookies
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 //link css etc file
-app.use(express.static(path.join(__dirname,"public")))
+app.use(express.static(path.join(__dirname, "public")))
 
 //making server
-app.get("/",function(req,res){
-    fs.readdir("./files",function(err,files){
-        res.render("index",{file:files});
+app.get("/", function (req, res) {
+    fs.readdir("./files", function (err, files) {
+        res.render("index", { file: files });
         console.log(files)
     })
-    
+
 })
+//storing data in files
+app.post("/post", function (req, res) {
+    fs.writeFile(`./files/${req.body.title.split(" ").join("")}.txt`, req.body.textarea, function (err) {
+        res.redirect("/")
+    })
+
+})
+
 
 app.listen(3000)
